ff6ed0f5e73be0ae1cd7e2088f622f0e4730fdd1
kritistha <kritistha020@gmail.com>
2025-07-17 16:06:48 +1000

    deploy

--- src/components/partials/footer-client.jsx
+++ src/components/partials/footer-client.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { Footer } from "./Footer.jsx"
+import Footer from "./Footer.jsx"
 
 const footerRoot = document.getElementById("footer-root")
 if (footerRoot) {
